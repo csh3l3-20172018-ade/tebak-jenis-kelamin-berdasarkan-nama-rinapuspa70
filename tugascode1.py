7970309b76bebeff3e75fa5478e190dfe11f7a79
rinapuspa70 <33598281+rinapuspa70@users.noreply.github.com>
2018-01-24 18:30:08 +0700

    Add files via upload

---
+++ tugascode1.py
@@ -0,0 +1,44 @@
+#menghitung jumlah huruf yang sering muncul jika dia perempuan
+def perempuan(p):
+    for i in range(0,len(p)):
+        r=0
+        if (p[i] == "i"):
+            r = r+1
+        elif (p[i] == "a"):
+            r = r+1
+        elif (p[i] == "u"):
+            r = r+1
+        elif (p[i] == "e"):
+            r = r+1
+        elif (p[i] == "t"):
+            r = r+1
+        elif (p[i] == "l"):
+            r = r+1
+    return r
+
+#menghitung jumlah huruf yang sering muncul jika dia laki-laki
+def laki(z):
+    n=0
+    for i in range(0,len(z)):
+        if (z[i] == "b"):
+            n = n+1
+        elif (z[i] == "d"):
+            n = n+1
+        elif (z[i] == "o"):
+            n = n+1
+    return n
+
+#menginputkan nama
+
+x = input('masukkan nama :')
+genlaki=laki(x)
+genperempuan=perempuan(x)
+
+#membandingkan jumlah huruf yang muncul
+
+if genlaki>genperempuan:
+    print("lelaki")
+elif genperempuan>genlaki:
+    print("perempuan")
+else: print("tidak diketahui")
+print("genlaki= ",genlaki,"genperempuan= ",genperempuan)
